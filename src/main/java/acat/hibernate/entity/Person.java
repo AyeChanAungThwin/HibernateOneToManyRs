9346a41ec223283f6c4064010d8609f6da68ae26
Aye Chan Aung Thwin <ayechanaungthwin@gmail.com>
2019-10-31 17:05:58 +0630

    updated column name

--- src/main/java/acat/hibernate/entity/Person.java
+++ src/main/java/acat/hibernate/entity/Person.java
@@ -27,7 +27,7 @@ public class Person extends BaseEntity<Long> implements Serializable {
 	@Column(name = "email")
 	private String email;
 	
-	@Column(name = "phNo")
+	@Column(name = "ph_no")
 	private String phNo;
 	
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "person", fetch = FetchType.EAGER)
